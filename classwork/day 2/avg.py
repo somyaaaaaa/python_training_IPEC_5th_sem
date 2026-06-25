8460406120722a16eb07f0115276c08b2e7fcf97
somya <somyasinghal03@gmail.com>
2026-06-25 23:05:16 +0530

    avg.py

---
+++ classwork/day 2/avg.py
@@ -0,0 +1,12 @@
+# to find out the average of five numbers
+
+#taking input from the user
+a = float(input("enter first number: "))
+b = float(input("enter second number: "))
+c = float(input("enter third number: "))    
+d = float(input("enter fourth number: "))
+e = float(input("enter fifth number: "))
+
+#calculating average
+avg = (a+ b+ c+d+e)/5
+print ("average of five numbers is :", avg)
