f18c8a2ae14a82e66c2772fc44cb79e605cf645a
somya <somyasinghal03@gmail.com>
2026-06-25 23:18:18 +0530

    velocity.py

---
+++ classwork/day 2/velocity.py
@@ -0,0 +1,7 @@
+#to find out the velocity of an object
+#taking input from the user
+d = float(input("enter distance (in meters): "))
+t = float(input("enter time (in seconds): "))
+#calculating velocity
+v = d/t
+print ("velocity of an object is :", v, "m/s")
